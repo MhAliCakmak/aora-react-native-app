475e4e9b8bebcda646adda1ad0a100ca457992c4
MhAliCakmak <202803055@ogr.cbu.edu.tr>
2024-09-12 22:04:45 +0300

    Refactor file structure and component names

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -5,7 +5,7 @@ import { icons } from "../../constants";
 
 const TabIcon = ({ icon, color, name, focused }) => {
   return (
-    <View className="items-center justify-center gap-2">
+    <View className="items-center justify-center gap-2 mt-2">
       <Image
         source={icon}
         resizeMode="contain"
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,13 +1,49 @@
-import { View, Text, SafeAreaView, FlatList, Image } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  Image,
+  RefreshControl,
+  Alert,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
+import { getAllPosts } from "../../lib/appwrite";
 
 const Home = () => {
+  const [data, setData] = React.useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchData = async () => {
+        setIsLoading(true);
+        try {
+          const response = await getAllPosts();
+          setData(response);
+        } catch (error) {
+          Alert.alert("Error", error.message);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+    };
+  }, []);
+
+  const [refreshing, setRefreshing] = React.useState(false);
+  
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
   return (
-    <SafeAreaView className="bg-primary">
+    <SafeAreaView className="bg-primary h-full">
       <FlatList
         data={[]}
         keyExtractor={(item) => item.$id}
@@ -20,38 +56,41 @@ const Home = () => {
           <View className="my-6 px-4 space-y-4">
             <View className="justify-between items-start flex-row mb-6">
               <View>
-              <Text className="font-pmedium text-sm text-gray-100">
-                Welcome to
-              </Text>
-              <Text className="text-2xl font-psemibold text-white">
-                Appwrite
-              </Text>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome to
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  Appwrite
+                </Text>
               </View>
-            <View className="mt-1.5">
-             <Image
-                source={images.logoSmall}
-                className="w-9 h-10"
-                resizeMode="contain"
-              />
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </View>
             </View>
-            </View>
-            <SearchInput/>
+            <SearchInput />
             <View className="w-full flex-1 pt-5 pb-8">
               <Text className="text-gray-100 text-lg font-pregular mb-3">
                 Latest Videos
               </Text>
-              <Trending posts={[{id:1},{id:2},{id:3}] ?? []}/>
+              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
             </View>
           </View>
         )}
-        ListEmptyComponent={()=>{
+        ListEmptyComponent={() => {
           return (
-           <EmptyState
+            <EmptyState
               title="No Videos Found"
               subtitle="Be the first one upload a video"
-           />
-          )
+            />
+          );
         }}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
     </SafeAreaView>
   );
